7e8717e21a3881df5ad36c9a837f9f0619ea3425
Natalia Tsaryova <natik_375@mail.ru>
2016-04-20 18:33:36 +0300

    Update script.js

--- GoIt hw/js_1,2/js/script.js
+++ GoIt hw/js_1,2/js/script.js
@@ -1,22 +1,49 @@
 
 
 function pow(a, b) {
-  var result = a;
+    var result = a;
+    var c;
 
-  for (var i = 1; i < b; i++) {
-    result *= a;
-  }
+    for (var i = 1; i < b; i++) {
+    	result = result * a;
+        
+    };
+    if(b > 0){
+         return result;
+    };
+    
+   if (b == 0) {
+        return 1;
+    };
+   
+ 
+if (b < 0) {
+     c = +b * (-1);
+    for(var j = 1; j < c; j++){
+        
+        result = result * a;
+    };
+    return 1 / result;
+};
+  
+     
+    
+      
+    
 
-  return result;
-}
+};
 
+    	
 var a = prompt("Enter your number", '');
 var b = prompt("Enter your exponent", '');
 
-if (b <= 1) {
-  alert("These aren't the" + b + "your looking for.." 
-  
-  );
-} else {
-  alert( pow(a, b) );
+
+
+
+   if (a % 1 == 0 && (b % 1 == 0)) {
+       alert ( pow(a, b) );
+
 }
+    else {
+        alert ("Enter an integer");
+    }
